bd2bb0a9283596433c07c8a278eef64cc68d1988
tsh <dr.tallin@gmail.com>
2017-06-10 22:21:09 +0300

    add threshold

--- mnist_test.py
+++ mnist_test.py
@@ -2,7 +2,6 @@ from mnist import MNIST
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn import svm
 
 mndata = MNIST('data')
 
@@ -11,16 +10,21 @@ images, labels = mndata.load_training()
 images = images[:5000]
 labels = labels[:5000]
 
-# arr = np.array(images[1700]).reshape((28, 28))
-# fig = plt.figure()
-# plt.imshow(arr, cmap='gray')
-# plt.show()
+def threshold(array):
+    for i, val in enumerate(array):
+        if val > 0:
+            array[i] = 1
+
+
 
 train_images, test_images,train_labels, test_labels = train_test_split(images, labels, train_size=0.8, random_state=0)
+for image in train_images:
+    threshold(image)
+
+
 img=np.array(train_images[1])
+plt.hist(img)
 img=img.reshape((28,28))
-
-clf = svm.SVC()
-clf.fit(train_images, train_labels)
-print(clf.score(test_images,test_labels))
-
+plt.show()
+# plt.imshow(img, cmap='gray')
+# plt.show()
