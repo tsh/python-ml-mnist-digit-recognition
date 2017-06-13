995e3ac4b303e87d76f17ec49eb1caf85e8d6947
Anton Nikulin <a_nikulin2@wargaming.net>
2017-06-13 19:03:02 +0300

    render template

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host=os.environ.get('HOST', '0.0.0.0'),
+            port=int(os.environ.get('PORT', '8000')),
+            debug=os.environ.get('DEBUG', False))
