a6e57f864d698aaafe7c78d7709d2d6534c93953
Anton Nikulin <a_nikulin2@wargaming.net>
2017-06-13 19:27:08 +0300

    send post

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/recognize_image', methods=['POST'])
+def recognize_image():
+    print (request.form)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('HOST', '0.0.0.0'),
