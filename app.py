525e374d75caea704b151ce2284b98d2e3a50d50
Anton Nikulin <a_nikulin2@wargaming.net>
2017-06-15 19:06:53 +0300

    parse pillow Image

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 import base64
 import re
+import io
+from PIL import Image
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
@@ -13,10 +15,11 @@ def hello_world():
 
 @app.route('/recognize_image', methods=['POST'])
 def recognize_image():
-    print (request.form)
-    image_data = re.sub(r'^data:image/.+;base64,', '', request.form['img'])
-    print (image_data)
+    img_data = re.sub(r'^data:image/.+;base64,', '', request.form['img'])
+    img_bytes = base64.b64decode(img_data)
+    img = Image.open(io.BytesIO(img_bytes))
     return jsonify({'status': 'ok'})
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('HOST', '0.0.0.0'),
